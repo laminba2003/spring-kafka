ba58ad16c4733a316b3f4317b6cd7998b01e1c5b
mamadou lamine ba <laminba2003@gmail.com>
2022-10-27 16:51:40 +0000

    improvements

--- src/main/java/com/spring/training/ApplicationConfig.java
+++ src/main/java/com/spring/training/ApplicationConfig.java
@@ -2,11 +2,19 @@ package com.spring.training;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListenerConfigurer;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistrar;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.support.serializer.DeserializationException;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
@@ -29,6 +37,23 @@ public class ApplicationConfig implements KafkaListenerConfigurer {
             log.error("validation occured with {} ", message);
             return message;
         };
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer, ConsumerFactory<Object, Object> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, consumerFactory);
+        factory.setCommonErrorHandler(new CommonErrorHandler() {
+            @Override
+            public void handleRecord(Exception exception, ConsumerRecord<?, ?> record, Consumer<?, ?> consumer, MessageListenerContainer container) {
+                if (exception.getCause() != null && exception.getCause() instanceof DeserializationException) {
+                    DeserializationException deserializationException = (DeserializationException) exception.getCause();
+                    log.error("cannot deserialize messsage with payload : {} ", new String(deserializationException.getData()));
+                }
+            }
+        });
+        return factory;
     }
 
 }
